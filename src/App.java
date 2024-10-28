66639941cad3a4b6c52e6b95f41d53927db4a96b
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-10-28 13:16:56 -0400

    fields

--- src/App.java
+++ src/App.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class App {
+public class App {
+    public static void main(String[] args) {
+
+
+        Library A = new Library();
+        A.name = "A";
+        A.addBooks(100);
+
+        Library B = new Library();
+        B.name = "B";
+        B.addBooks(150);
+
+        System.out.println("Total books in all libraries: " + Library.getTotalBooks() );
+    }
 }
--- src/Library.java
+++ src/Library.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Library {
+public class Library {
+
+    public String name;
+    public static int totalBooks;
+
+    public void addBooks(int numBooks){
+        totalBooks += numBooks;
+    }
+
+    public static int getTotalBooks(){
+        return totalBooks;
+    }
 }
